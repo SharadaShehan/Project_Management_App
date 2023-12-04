1931db8c0df8a31559c8a2804e77c6cda33ac7b7
Sharada Shehan <sharadashehan6@gmail.com>
2023-12-05 02:14:13 +0530

    ws

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,99 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import { ApolloServer } from '@apollo/server'
+import { createServer } from 'http'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import bodyParser from 'body-parser'
+import express from 'express'
+import { WebSocketServer } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
+import { PubSub } from 'graphql-subscriptions'
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
+const port = 3000
+let counter = 0
+
+const typeDefs = `
+    type Query {
+        foo: String!
+    }
+    type Mutation {
+        scheduleOperation(name: String!): String!
+    }
+    type Subscription {
+        operationFinished: Operation!
+    }
+
+    type Operation {
+        name: String!
+        endDate: String!
+        value: Int!
+    }
+`
+
+const pubSub = new PubSub()
+
+const mockLongLastingOperation = (name) => {
+  setTimeout(() => {
+    counter++
+    pubSub.publish('OPERATION_FINISHED', { operationFinished: { name, endDate: new Date().toDateString(), value: counter } })
+  }, 1000)
+}
+
+const resolvers = {
+  Mutation: {
+    scheduleOperation (_, { name }) {
+      mockLongLastingOperation(name)
+      return `Operation: ${name} scheduled!`
+    }
+  },
+  Query: {
+    foo () {
+      return 'foo'
+    }
+  },
+  Subscription: {
+    operationFinished: {
+      subscribe: () => pubSub.asyncIterator(['OPERATION_FINISHED'])
+    }
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const app = express()
+const httpServer = createServer(app)
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql'
 })
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+const wsServerCleanup = useServer({ schema }, wsServer)
+
+const apolloServer = new ApolloServer({
+  schema,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart () {
+        return {
+          async drainServer () {
+            await wsServerCleanup.dispose()
+          }
+        }
+      }
+    }
+  ]
+})
+
+await apolloServer.start()
+
+app.use('/graphql', bodyParser.json(), expressMiddleware(apolloServer))
+
+httpServer.listen(port, () => {
+  console.log(`🚀 Query endpoint ready at http://localhost:${port}/graphql`)
+  console.log(`🚀 Subscription endpoint ready at ws://localhost:${port}/graphql`)
 })
