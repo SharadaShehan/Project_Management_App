6c8524a9769d4862267da8270a546e0035f3ee9e
Sharada Shehan <sharadashehan6@gmail.com>
2024-05-30 12:01:11 +0530

    invalid import

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { ApolloProvider } from '@apollo/client';
 import client from './client';
-import Basic from './src/basic';
 import AppNavigator from './src/AppNavigator';
 
 export default function App() {
--- frontend/client.js
+++ frontend/client.js
@@ -5,7 +5,7 @@ import { getMainDefinition } from '@apollo/client/utilities';
 import { createClient } from 'graphql-ws';
 import { SERVER_IP_ADDR, SERVER_PORT } from '@env';
 
-SERVER_IP_ADDR = '192.168.1.3';
+SERVER_IP_ADDR = '192.168.1.4';
 
 console.log(`SERVER_IP_ADDR: ${SERVER_IP_ADDR}`);
 console.log(`SERVER_PORT: ${SERVER_PORT}`);
