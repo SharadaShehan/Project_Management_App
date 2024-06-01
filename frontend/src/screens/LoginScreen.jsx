c0dff16a762091f557b504d3c2013137930fa30a
Sharada Shehan <sharadashehan6@gmail.com>
2024-06-01 09:59:38 +0530

    swapping between login and signup

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import LoginForm from '../components/LoginForm';
+import SignUp from '../components/SignUp';
+import { useState } from 'react';
 
 const LoginScreen = ({ navigation }) => {
+  const [ loginPage, setLoginPage ] = useState(true);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 5 }}> 
-      <LoginForm navigation={navigation} />
+      {loginPage ? <LoginForm navigation={navigation} /> : <SignUp navigation={navigation} />}
+      <Button
+        title={loginPage ? 'Sign Up' : 'Login'}
+        onPress={() => setLoginPage(!loginPage)}
+      />
       <Button
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
