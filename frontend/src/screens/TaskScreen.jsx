c4ce81fd9fbee08db221fd1f29a39fdba10198d8
Sharada Shehan <sharadashehan6@gmail.com>
2024-06-06 19:59:25 +0530

    content rendering modified and task delete feature added

--- frontend/src/screens/TaskScreen.jsx
+++ frontend/src/screens/TaskScreen.jsx
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, TouchableOpacity, FlatList, ScrollView } from 'react-native';
+import { View, Text, Button, TouchableOpacity, FlatList, ScrollView, Image, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { TASK_QUERY } from '../queries/Queries';
-import { useQuery } from '@apollo/client';
+import { DELETE_TASK_MUTATION } from '../queries/Mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { UserGlobalState } from '../layout/UserState';
 
 const TaskScreen = ({navigation, route}) => {
-    const taskId = route.params.id;
+    const taskId = route.params.task.id;
+    const phase = route.params.phase;
     const { userData } = UserGlobalState();
-    const { data:taskData, loading:taskLoading, error:taskError } = useQuery(TASK_QUERY, { variables: { id: taskId } });
-
-    const renderItem = ({ item }) => (
-        <TouchableOpacity style={styles.taskContainer} disabled={phaseData.phase.phaseAdmins.map((admin) => admin.id).includes(userData.id) ? false : true}
-            onPress={() => { console.log('Task Pressed') }}>
-            <Text style={{ fontWeight: 'bold' }}>{item.title}</Text>
-            <Text style={{ fontSize: 12, color: '#434343' }}>{item.status}</Text>
-            <Text>{item.description}</Text>
-            {item.endDate && !item.endTime && <Text>Due: {item.endDate}</Text>}
-            {item.endDate && item.endTime && <Text>Due: {item.endDate} at {item.endTime}</Text>}
-            {item.taskAssignees.length === 0 && <Text>No Assignees</Text>}
-            {item.taskAssignees.length > 0 && (
-                <View>
-                    <Text>Assignees:</Text>
-                    {item.taskAssignees.map((assignee) => (
-                        <View style={styles.memberContainer} key={assignee.username+'0'}>
-                            <Text style={{ fontWeight: 'bold' }} key={assignee.username+'1'}>{assignee.firstName} {assignee.lastName}</Text>
-                            <Text style={{ fontSize: 12, color: '#434343' }} key={assignee.username+'3'}>{assignee.username}</Text>
-                        </View>
-                    ))}
-                </View>
-            )}
-        </TouchableOpacity>
-    );
+    const { data:taskData, loading:taskLoading, error:taskError } = useQuery(TASK_QUERY, { variables: { id: taskId }, fetchPolicy: 'network-only' });
+    const [deleteTask] = useMutation(DELETE_TASK_MUTATION);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -44,23 +24,55 @@ const TaskScreen = ({navigation, route}) => {
                             <Text style={styles.taskTitle}>{taskData.task.title}</Text>
                             <Text style={[styles.taskStatus, { color: taskData.task.status === 'Active' ? '#009900' : '#FF0000' }]}>{taskData.task.status}</Text>
                             {taskData.task.description && <Text style={styles.taskDescription}>{taskData.task.description}</Text>}
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 16 }}>
-                                <FlatList
-                                    data={taskData.task.taskAssignees}
-                                    renderItem={renderItem}
-                                    keyExtractor={(item) => item.id}
-                                    ListHeaderComponent={() => (<Text style={{ fontWeight: 'bold', fontSize: 17, marginTop: 10, alignSelf: 'center', marginBottom: 3 }}>Assignees</Text>)}
-                                />
-                                <TouchableOpacity style={[styles.lowerButton, { backgroundColor: '#007BFF', borderRadius: 8 }]} onPress={() => { console.log('Add Task') }}>
-                                    <Text style={styles.lowerButtonText}>Assign/Unassign Task</Text>
-                                </TouchableOpacity>
-                            </View>
+                            {taskData.task.endDate && !taskData.task.endTime && <Text style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 5, alignSelf: 'center' }}>Due :- {taskData.task.endDate}</Text>}
+                            {taskData.task.endDate && taskData.task.endTime && <Text style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 5, alignSelf: 'center' }}>Due :- {taskData.task.endDate} at {taskData.task.endTime}</Text>}
+                            <Text style={{ fontWeight: 'bold', fontSize: 17, marginTop: 10, alignSelf: 'center' }}>Assignees</Text>
+                            {taskData.task.taskAssignees.length === 0 && <Text style={{ fontWeight: 'bold', color: '#aaa', fontSize: 17, marginTop: 10, alignSelf: 'center', marginBottom: 8 }}>No Assignees For This Task</Text>}
+                            {taskData.task.taskAssignees.length > 0 && (
+                                <View>
+                                    {taskData.task.taskAssignees.map((assignee) => (
+                                        <View style={[styles.memberContainer,{ flexDirection: 'row', alignItems: 'center' }]} key={assignee.username+'0'}>
+                                            <Image source={assignee.imageURL ? { uri: assignee.imageURL } : require('../../images/profile.webp')} style={{ width: 25, height: 25, borderRadius: 25, marginLeft: 5 }} />
+                                            <View style={{ marginLeft: 15 }} key={assignee.username+'2'}>
+                                                <Text style={{ fontWeight: 'bold' }} key={assignee.username+'1'}>{assignee.firstName} {assignee.lastName}</Text>
+                                                <Text style={{ fontSize: 12, color: '#434343' }} key={assignee.username+'3'}>{assignee.username}</Text>
+                                            </View>
+                                        </View>
+                                    ))}
+                                </View>
+                            )}
+                            <TouchableOpacity style={[styles.lowerButton, { backgroundColor: '#007BFF', borderRadius: 8 }]} onPress={() => navigation.navigate('UpdateTaskAssignees', { task: taskData.task, phase: route.params.phase })}>
+                                <Text style={styles.lowerButtonText}>Assign/Unassign Task</Text>
+                            </TouchableOpacity>
                         </View>
                     )}
                     <TouchableOpacity style={[styles.lowerButton, { backgroundColor: '#007BFF', borderRadius: 8, marginTop: 8 }]} onPress={() => { console.log('Edit Task') }}>
                         <Text style={styles.lowerButtonText}>Edit Task</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={[styles.lowerButton, { backgroundColor: '#dd0000', borderRadius: 8, marginTop: 8 }]} onPress={() => { console.log('Delete Task') }}>
+                    <TouchableOpacity style={[styles.lowerButton, { backgroundColor: '#dd0000', borderRadius: 8, marginTop: 8 }]} onPress={() => {
+                        // prompt user to confirm deletion
+                        Alert.alert(
+                            'Delete Task',
+                            'Are you sure you want to delete this task?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log('Cancelled'),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'Delete', onPress: async () => {
+                                        try {
+                                            await deleteTask({ variables: { id: taskId } });
+                                            navigation.goBack();
+                                        } catch (error) {
+                                            Alert.alert('Error', error.message);
+                                        }
+                                    }
+                                }
+                            ]
+                        );
+                    }}>
                         <Text style={styles.lowerButtonText}>Delete Task</Text>
                     </TouchableOpacity>
                 </ScrollView>
@@ -121,6 +133,13 @@ const styles = {
         selectedItemText: {
             color: '#fff',
         },
+    taskContainer: {
+        backgroundColor: '#eee',
+        padding: 10,
+        margin: 5,
+        marginHorizontal: 0,
+        borderRadius: 5,
+    },
     memberContainer: {
         backgroundColor: '#eee',
         padding: 10,
